86aa4359e49d667d6e13dfe83508e837d01587f8
muhreynaldii <reynaldi180101@gmail.com>
2022-04-08 22:09:04 +0700

    adjust get car

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,38 +1,57 @@
-import React, { Fragment,useEffect,useState } from 'react'
-import { Container } from 'reactstrap';
-import CardCars from '../../components/Card/CardCars';
-import FormPencarian from './../../components/FormPencarian/FormPencarian';
-import axios from 'axios';
+import React, { Fragment, useEffect, useState } from "react";
+import { Container } from "reactstrap";
+import CardCars from "../../components/Card/CardCars";
+import FormPencarian from "./../../components/FormPencarian/FormPencarian";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function Search() {
   const [dataList, setDataList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const params = useParams();
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        "https://rent-cars-api.herokuapp.com/admin/car"
+      );
+      setDataList(res.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    axios.get('https://rent-cars-api.herokuapp.com/admin/car')
-      .then(res => {
-        console.log(res.data);
-        setDataList(res.data);
-      })
-  },[])
+    setIsLoading(true);
+    getData();
+  }, []);
   return (
     <Fragment>
-      <Container fluid style={{backgroundColor: '#F1F3FF', height:'266px'}}>
-      </Container>
-        <FormPencarian/>
+      <Container
+        fluid
+        style={{ backgroundColor: "#F1F3FF", height: "266px" }}
+      ></Container>
+      <FormPencarian />
       <Container>
-        <CardCars
-         />
-        {dataList.map(item =>(
-          <CardCars
-          id={item?.id}
-          name={item?.name}
-          image={item?.image}
-          price={item?.price}
-          className="m-5"
-          />
-          ))}
-       </Container>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {dataList?.map((item) => {
+              return (
+                <CardCars
+                  id={item?.id}
+                  name={item?.name}
+                  image={item?.image}
+                  price={item?.price}
+                />
+              );
+            })}
+          </div>
+        )}
+      </Container>
     </Fragment>
-  )
+  );
 }
 
 export default Search;
